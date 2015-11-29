8c09a42071171a7b9e450700d475d505ab92123e
jerome <fanniojerome@free.fr>
2015-11-29 14:28:44 +0100

    modif kata

--- src/ArrayMaker.js
+++ src/ArrayMaker.js
@@ -1,7 +1,14 @@
 function arrayMaker(max) {
-  if(isNaN(max)) {
+  if (isNaN(max)) {
     return null;
-  } else if(max === 0) {
+  } else if (max === 0) {
     return [];
+
+  } else {
+    var tableau = [];
+    for (var i = 1; i <= max; i++) {
+      tableau.push(i);
+    }
+    return tableau;
   }
 }
